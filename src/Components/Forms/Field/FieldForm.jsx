9c0bef280b55996e7fb0620c548ab96b8e5c8081
Fredrik Olsvold <fredrik.olsvold@visavi.com>
2021-04-15 14:47:02 +0200

    Feat: Added function with button press to add FieldForm components in FormWrapper components

--- src/Components/Forms/Field/FieldForm.jsx
+++ src/Components/Forms/Field/FieldForm.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { MenuItem, TextField } from "@material-ui/core";
+import React from "react";
 
 function FieldForm() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <>
+      <TextField id="select" label="Field Type" select>
+          {/* Map MenuItems based on type options accessible from data source 
+            eg. data.types.map(t => {
+            <MenuItem value={t.value}>{t.name}</MenuItem>
+
+            })
+          */}
+        <MenuItem value="1">Type 1</MenuItem>
+        <MenuItem value="2">Type 2</MenuItem>
+        <MenuItem value="3">Type 3</MenuItem>
+      </TextField>
+      <TextField id="standard-basic" label="Label"/>
+    </>
+  );
 }
 
-export default FieldForm
+export default FieldForm;
