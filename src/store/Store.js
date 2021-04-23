aa2f12d11aa3a66277a543554c66671a16e4205f
Daniel Bjørke <daniel.bjorke@visavi.com>
2021-04-23 08:19:59 +0200

    merge from danieleditbuttons

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,9 @@
 import {atom} from "recoil";
-import {workOrderObject} from "../../src/api/getTemplate";
 
 //Atom that stores chosen window title sections in a list. Global state
 export const windowTitleAtom = atom({
 key: "windowTitleAtom",
-default: workOrderObject.title,
+default: [],
 });
 
 //Atom that stores chosen content sections in a list. Global state
